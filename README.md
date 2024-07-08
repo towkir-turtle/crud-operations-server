8a782e9e81c717bcd0b94b32ed722635abd38565
towkirahmed <towkir.webdeveloper@gmail.com>
2024-07-08 20:38:26 +0600

    database connected

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # crud-operations-server
+# Steps:
+1. Create a folder with project name, open folder with your code editor, and then open terminal. 
+2. Use the npm init command to create a package.json file for your application.
+2. Install necessary dependencies for your project.
+3. Create index.js file and configure server.
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -8,8 +8,19 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('crud server is running!')
+mongoose
+  .connect(
+    "mongodb+srv://crudServerUser:O7E0YCqw7arq4cZw@cluster0.pm0p0t4.mongodb.net/crud-server?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    console.log("database connected");
+  })
+  .catch(() => {
+    console.log("connection failed!");
+  });
+
+app.get("/", (req, res) => {
+  res.send("crud server is running!");
 });
 
 app.listen(port, () => {
